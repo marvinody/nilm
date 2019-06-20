69ad71e9a9cfe13d50ec20f7316e23c887fb9d09
Manuel Bugallo <marvinody@gmail.com>
2019-06-20 18:51:27 -0400

    feat: updated schema

--- priv/repo/migrations/20190619210433_create_users.exs
+++ priv/repo/migrations/20190619210433_create_users.exs
@@ -5,10 +5,10 @@ defmodule Nilm.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :name, :string
       add :email, :string
-      add :bio, :string
-
+      add :password_hash, :string
       timestamps()
     end
 
+    create unique_index(:users, [:email])
   end
 end
